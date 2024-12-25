e7f2c7eb760e7f3244ccd5dbe73dfe64773f0166
Milad Azami <miladazami120@gmail.com>
2024-12-25 20:57:20 +0330

    Payment page

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,10 +1,27 @@
-import React from 'react'
+"use client";
+
+import useQuery from "@/core/hooks/query";
+import Link from "next/link";
+import { useEffect } from "react";
 
 function PaymentPage() {
-    
+  const { getQuery } = useQuery();
+
+  const status = getQuery("status");
+
+  if (status === "success")
+    return (
+      <div>
+        <p>پرداخت شما با موفقیت انجام شد</p>
+        <Link href="/profile/my-tours">برو به پروفایل</Link>
+      </div>
+    );
+
   return (
-    <div>PaymentPage</div>
-  )
+    <div>
+      <p>پرداخت انجام نشد</p>
+    </div>
+  );
 }
 
-export default PaymentPage
+export default PaymentPage;
